4027ed534c4f6265334e1984d3b1e17cd143be26
Tiago Nunes <tiagosbnunes@gmail.com>
2013-02-26 23:25:58 +0000

    Add initializr assets (responsive bootstrap)

--- emif/emif/settings.py
+++ emif/emif/settings.py
@@ -73,7 +73,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.abspath('./static'),
+    os.path.abspath('./emif/static'),
     os.path.abspath('./apps/seantis-questionnaire/questionnaire/static/')
 )
 
