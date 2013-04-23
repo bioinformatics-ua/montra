6af37a4125a8698f0f1103379218fc740957793f
Luís A. Bastião Silva <luis.kop@gmail.com>
2013-04-23 22:13:00 +0100

    - Improve the test script to add dummy data - Added script to remove documents from Solr

--- emif/insert_script.py
+++ emif/insert_script.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
 import md5
 import random
 
@@ -19,7 +22,9 @@ import urllib2
 
 url = 'http://127.0.0.1:8000/api/insert'
 
-institutionnames = ["Hospital", "Joao", "Porto", "Aveiro", "Lisboa", "Faro", "Portimao"]
+institutionnames = ["Hospital", "Medical Department", "Research Center", "Pharmacy", "Hopital", "Ziekenhuis" ]
+
+databasenames =["heartattack", "cardiomyopathy", "Coronary heart disease", "Valvular heart disease", "Peripheral arterial disease"]
 
 location = ["Paris", "Roterdao", 
 "Porto", "Aveiro", "Lisboa", "Faro", "Portimao", "Brussels", "London", 
@@ -30,18 +35,32 @@ data = {
         'created_t':'2013-04-17 12:09:32.334053',
         'location_t':'Aveiro',
         'institution_name_t':'IEETA',
-        'contact_scientific_t':'IEETA',
-        'contact_administrative_t':'IEETA',
+        'contact_scientific_t':'jlo@ua.pt',
+        'contact_administrative_t':'jlo@ua.pt',
         'type_t':'researchcohorts',
         'id':'a10815736f733d04d8e0aa65fe37',
         'user_t' :'bastiao',
-        'text_t': 'ieeta ieeta bastiao emif',
-        'database_name_t':'IEETA'}
+        'text_t': 'ieeta ieeta bastiao emif cardiomyopathy Coronary heart attack',
+        'total_number_subjects_t': '20000',
+        'ethical_committee_t': '[No]',
+        'publically_doc_procedure_t': '[No]',
+        'ethical_committee_t': '[No]',
+        'number_active_patients_jan2012_t': '200',
+        'average_time_follow_up_t': '130',
+        'assess_prevalence_prevalence_t': 'Brufen Beneron',
+        'literature_papers_t': "Luis A. Bastiao Silva, Carlos Costa, Jose Luis Olveira. A Secure PACS Cloud Archive in CARS 2011, Berlin, Germany ",
+        'population_description_t':'Fat, Headcache'}
 
 import pysolr
+import random
 solr = pysolr.Solr('http://localhost:8983/solr', timeout=10)
 for i in range(10):
-	data['database_name_t'] = 'IEETA' + str(i)
+        index_db = random.randint(1, len(databasenames))
+        index_institutionnames = random.randint(1, len(institutionnames))
+        index_locations = random.randint(1, len(location))
+
+	data['database_name_t'] = institutionnames[index_institutionnames-1] + " " + location[index_locations-1] + " " +databasenames[index_db-1]
+        data['location_t'] = location[index_locations]
 	data['id'] = generate_hash()
 	solr.add([data])
 	
