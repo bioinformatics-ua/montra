018781414ce322559609d45cb1aa02efa5f19c9e
Luís A. Bastião Silva <luis.kop@gmail.com>
2013-05-07 14:37:05 +0100

    fetch by pmid is done now

--- emif/emif/literature.py
+++ emif/emif/literature.py
@@ -3,16 +3,29 @@
 
 from pprint import pprint
 from Bio import Entrez
- 
-Entrez.email = "you@example.com"
-Entrez.tool = "test-tool"
- 
-pmid = 23225384
-handle = Entrez.efetch(db='pubmed', id=pmid, retmode='xml')
-#print(handle.read())
-record = Entrez.read(handle)
- 
-pprint(record[0]['PubmedData'])
 
-pprint(record[0]['MedlineCitation']['Article']['ArticleTitle'])
-pprint(record[0]['MedlineCitation']['Article']['Abstract'])
+
+
+def fetch_by_pmid(pmid):
+
+	Entrez.email = "you@example.com"
+	Entrez.tool = "test-tool"
+	 
+	#pmid = 23225384
+	handle = Entrez.efetch(db='pubmed', id=pmid, retmode='xml')
+	#print(handle.read())
+	record = Entrez.read(handle)
+	 
+	#pprint(record[0]['PubmedData'])
+
+	#pprint(record[0]['MedlineCitation']['Article']['ArticleTitle'])
+	title = record[0]['MedlineCitation']['Article']['ArticleTitle']
+	#pprint(record[0]['MedlineCitation']['Article']['Abstract']['AbstractText'])
+	abstract = record[0]['MedlineCitation']['Article']['Abstract']['AbstractText']
+	abstract_text = ""
+	for a in abstract:
+		abstract_text += a
+	print abstract_text
+	return (title, abstract_text)
+
+#pprint(fetch_by_pmid(23225384))
