99c2e6ab2beed2ad054b61f70a34bede29ad22d9
Luís A. Bastião Silva <luis.kop@gmail.com>
2013-09-20 15:32:43 +0100

    added help to the delete script

--- emif/utils/delete_questionnaire.py
+++ emif/utils/delete_questionnaire.py
@@ -1,9 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2013 Luís A. Bastião Silva and Universidade de Aveiro
+#
+# Authors: Luís A. Bastião Silva <bastiao@ua.pt>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
 from questionnaire.models import *
 from django.shortcuts import render_to_response, get_object_or_404
 import sys
 
+ 
 
-to_delete = [9, 8, 6, 5, 4, 3, 2]
+# Uncomment this and add your list of ids to remove (of Questionnaire model)
+#to_delete = [9, 8, 6, 5, 4, 3, 2]
 
 for rem in to_delete:
 	qu = get_object_or_404(Questionnaire, id=rem)
