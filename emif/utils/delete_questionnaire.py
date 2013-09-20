54ab060574eac3a4b4b4859949c841c9fbecbe9f
Luís A. Bastião Silva <luis.kop@gmail.com>
2013-09-20 15:25:47 +0100

    - Remove imports from menu - Fix script to remove a list of templates

--- emif/utils/delete_questionnaire.py
+++ emif/utils/delete_questionnaire.py
@@ -1,25 +1,30 @@
 from questionnaire.models import *
 from django.shortcuts import render_to_response, get_object_or_404
-
-qu = get_object_or_404(Questionnaire, id=7)
-
+import sys
 
 
-qsets = qu.questionsets()
+to_delete = [9, 8, 6, 5, 4, 3, 2]
 
-for qs in qsets:
-	print "iterate questions"
-	expected = qs.questions()
-	
-	for q in expected:
-		print "iterate choices"
-		print q.choices
-		try:
-			for c in q.choices:
-				c.delete()
-		except:
-			pass
-		q.delete()
-	qs.delete()
+for rem in to_delete:
+	qu = get_object_or_404(Questionnaire, id=rem)
 
-qu.delete()
+
+
+	qsets = qu.questionsets()
+
+	for qs in qsets:
+		print "iterate questions"
+		expected = qs.questions()
+		
+		for q in expected:
+			print "iterate choices"
+			print q.choices
+			try:
+				for c in q.choices:
+					c.delete()
+			except:
+				pass
+			q.delete()
+		qs.delete()
+
+	qu.delete()
