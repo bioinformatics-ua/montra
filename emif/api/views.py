0bcab98027648c880a6868d97287213a5f1aa5a5
Luís A. Bastião Silva <luis.kop@gmail.com>
2013-10-30 23:59:52 +0000

    Give a try on EMIF utils

--- emif/api/views.py
+++ emif/api/views.py
@@ -49,7 +49,7 @@ from api.models import *
 
 
 # Import pubmed object
-from utils.pubmed import PubMedObject
+from emif.utils.pubmed import PubMedObject
 
 class JSONResponse(HttpResponse):
     """
