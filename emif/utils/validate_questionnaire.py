7c25567c059b7b09d2b9f50e9b9c6df0b4cd6dbe
Luís A. Bastião Silva <luis.kop@gmail.com>
2013-12-03 19:01:59 +0000

    Tool to validate the Questionnaire

---
+++ emif/utils/validate_questionnaire.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2013 Luís A. Bastião Silva and Universidade de Aveiro
+#
+# Authors: Luís A. Bastião Silva <bastiao@ua.pt>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+
+from django.http import HttpResponse, HttpResponseRedirect
+
+from questionnaire.models import *
+from django.shortcuts import render_to_response, get_object_or_404
+import sys
+
+from searchengine.models import *
+
+rem = 1
+    
+
+
+qu = get_object_or_404(Questionnaire, id=rem)
+
+qsets = qu.questionsets()
+
+for qs in qsets:
+    
+    expected = qs.questions()
+    for q in expected:
+        slugs = Slugs.objects.filter(description=q.text)
+        if len(slugs)!=1:
+            print "Error (multiple slugs to the description): " +  q.number
+            for s in slugs:
+                print s.slug1 + "| " + s.description + "| " + str(s.question.pk)
+            continue
+        s = slugs[0]
+        if (s.slug1 != q.slug):
+            print "Error (slug1!=slug): " + q.number
+            continue
+        if (s.question.pk!=q.pk):
+            print "Error (q.pk!=pk): " + q.number
+            continue
+
+
