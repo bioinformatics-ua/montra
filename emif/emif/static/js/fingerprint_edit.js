ed6ad7fc2641318393ab06c2198fa29b9997e0b6
Rui Mendes <ruidamendes@ua.pt>
2014-01-04 17:26:32 +0000

    Toggle button to enable/disabled edit option in fingerprint edit #69

--- emif/emif/static/js/fingerprint_edit.js
+++ emif/emif/static/js/fingerprint_edit.js
@@ -4,3 +4,14 @@ if ( $("#edit_db_inputs").val() == '1' ) {
         $("#qform input").prop("disabled", true);
     }
 
+function edit_db_option(){
+
+    console.log($("#edit_db_inputs").val());
+    if ( $("#edit_db_inputs").val() == '0' ) {
+        $("#qform input").prop("disabled", true);
+        $("#edit_db_inputs").val('1');
+    } else if ( $("#edit_db_inputs").val() == '1' ) {
+        $("#qform input").prop("disabled", false);
+        $("#edit_db_inputs").val('0');
+    }
+}
