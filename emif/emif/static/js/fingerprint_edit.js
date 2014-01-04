c927f658b5de692e9efa68411ca5fd36536699d5
Rui Mendes <ruidamendes@ua.pt>
2014-01-04 17:25:47 +0000

    Read only view by default in fingerprint edit #69

---
+++ emif/emif/static/js/fingerprint_edit.js
@@ -0,0 +1,6 @@
+/* Option to active edit fingerprint option */
+if ( $("#edit_db_inputs").val() == '1' ) {
+    console.log($("#edit_db_inputs").val());
+        $("#qform input").prop("disabled", true);
+    }
+
