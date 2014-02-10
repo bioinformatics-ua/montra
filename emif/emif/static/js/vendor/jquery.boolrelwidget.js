ab7990225456a9dd5dd35723d7465b966052c604
Ricardo Ribeiro <ribeiro.r@ua.pt>
2014-02-10 18:36:28 +0000

    Started working on visual layout  for #71

--- emif/emif/static/js/vendor/jquery.boolrelwidget.js
+++ emif/emif/static/js/vendor/jquery.boolrelwidget.js
@@ -6,7 +6,6 @@
 
 The MIT License (MIT)
 
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
@@ -25,18 +24,160 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 # */
-
 (function ($) {
     $.fn.boolrelwidget = function (options) {
         // so we dont lose track of who we are, and used to ensure chainability
         var self = this;
-        var loose_variables = [];
-        var boolean_definitions = [];
+        var basic_blocks = [];
+        var mastergroup=null;
         // Default Options
         var settings = $.extend({
             expand_text:'To define the relations between the terms click here.',
             collapse_text:'Click here to close this panel.'
         }, options);
+        
+        var group1 = new BooleanGroup('A');
+        var group2 = new BooleanGroup('B');
+        var group3 = new BooleanGroup('C');
+
+        var group_xpto = new BooleanGroup(group1);
+        group_xpto.addBoolean(BOOL['OR'],group2);
+        group_xpto.addBoolean(BOOL['OR'],group3);
+        var group_xpto2 = new BooleanGroup(new BooleanGroup('Z'));
+        group_xpto2.addBoolean(BOOL['XOR'],group_xpto);
+
+        
+        mastergroup = new BooleanGroup(group_xpto2);
+        
+        var funcs = {
+            push: function (str) {
+                
+                if(this.getIndex(str)!= -1){
+                    console.warn('Variable ' + str + ' already on basic blocks pool.');  
+                    return null;
+                }
+                
+                var block = new BooleanGroup(str);
+                basic_blocks.push(block);
+                console.warn('Pushed new variable ' + str + ' to basic blocks pool.');
+
+                this.draw();
+                
+                return block;
+            },
+            splice: function (str) {
+                var id = this.getIndex(str)
+                if(id < 0){
+                   console.warn('No basic block ' + str + ' from basic blocks pool to slice out.');
+                   return null; 
+                }
+                var block = basic_blocks.splice();
+                console.warn('Sliced out variable ' + str + ' from basic blocks pool.');
+
+                this.draw();
+                
+                return block;
+            },
+            // I define this manually because IE<=8 js lists doesnt have the method indexOF()
+            getIndex: function(element){
+                var i = 0;
+                for(i=0;i<basic_blocks.length;i++){
+                    // We must check this is a "empty" container with only one element (the one we want).
+                    if(basic_blocks[i].containsOnly(element))
+                        return i;
+                }
+                return -1;
+            },
+            draw: function(){
+                // Drawing concepts
+                var little_boxes = [];
+                
+                var i=0;
+                for(i=0;i<basic_blocks.length;i++){
+                    little_boxes.push('<span unselectable="on" id="boolrelwidget-block');
+                    little_boxes.push(basic_blocks[i].id);
+                    little_boxes.push('" class="boolrelwidget-block">');
+                    little_boxes.push('<span class="boolrelwidget-block-inner">');
+                    little_boxes.push(basic_blocks[i].variables[0]);
+                    little_boxes.push('</span>');
+                    little_boxes.push('<span class="boolrelwidget-delete">');
+                    little_boxes.push('X');
+                    little_boxes.push('</span>');
+                    little_boxes.push('</span>');
+
+                }
+                
+                if(little_boxes.length == 0)
+                    $('#boolrelwidget-basicblocks').html("You must add concepts before manipulating their logic.");    
+                else
+                    $('#boolrelwidget-basicblocks').html(little_boxes.join(''));
+                
+                // Drawing query itself(if any already)
+                
+                if(mastergroup && mastergroup.variables.length>0){
+                    var big_box = [];
+                    var i = 0;
+                    this.harvest(mastergroup, big_box, 0);
+                    
+                    $('#boolrelwidget-query').html(big_box.join(''));
+                    
+                } else {
+                    $('#boolrelwidget-query').html('Drag and Drop concepts here to start building a query...');
+                }
+            },
+            // This recursive functions runs down in the BooleanGroups and puts everything on the big box.
+            // I pass a counter to be able to style differently (so i know the recursion level couldnt find a better way to style it different
+            harvest: function(something, big_box, counter){
+                if(typeof something == 'string' || something instanceof String){
+                    big_box.push(something);
+                }
+                else if(something instanceof BooleanGroup){
+                    var k = 0;
+                    
+                    // First one doesnt have a operator associated
+                    big_box.push('<div class="boolrelwidget-query-box-outer">');
+                    if(counter%2 == 0)
+                        big_box.push('<div class="boolrelwidget-odd boolrelwidget-query-box">');
+                    else big_box.push('<div class="boolrelwidget-even boolrelwidget-query-box">');
+                    this.harvest(something.variables[k++], big_box, counter+1);
+   
+                    // All others in the big box have
+                    for(k=1;k<something.variables.length;k++){
+                        // Add relation box
+                        big_box.push(this.operator_selectbox(something.relations[k-1].name, counter));
+                        
+                        this.harvest(something.variables[k], big_box, counter+1);
+                    }
+                    big_box.push("</div>");
+                    if(counter%2 == 0)
+                        big_box.push('<div class="boolrelwidget-odd boolrelwidget-query-dropper">[drop]</div><div class="boolrelwidget-odd boolrelwidget-query-delete">X</div>');
+                    else 
+                        big_box.push('<div class="boolrelwidget-even boolrelwidget-query-dropper">[drop]</div><div class="boolrelwidget-even boolrelwidget-query-delete">X</div>');
+                    
+                    big_box.push('</div>');
+                } else {
+                    console.error(something+' cant be put in the big_box, because its not of type string nor BooleanGroup.');
+                }
+            },
+            operator_selectbox: function(selected, counter){
+                var select = [];
+
+                select.push('<span class="boolrelwidget-query-operator"><select class="boolrelwidget-select">');
+
+                
+                for(var index in BOOL) {
+                    if(BOOL[index].name == selected)
+                        select.push('<option selected="selected">');
+                    else
+                        select.push('<option>');
+                    select.push(BOOL[index].name);
+                    select.push('</option>');
+                }                
+                select.push('</select></span>');
+                
+                return select.join('');
+            }
+        };
 
         // Murphy's law says people will forget to pass a div container, give helpful hint
         if (!this.is('div')) {
@@ -51,9 +192,11 @@ THE SOFTWARE.
         self = self.html('');
         
         // Now lets add the toolbar
-        self = self.append('<ul id="boolrelwidget-expand" class="boolrelwidget-menu-container"><li class="boolrelwidget-menu"><div class="boolrelwidget-arrow-l"><div class="boolrelwidget-arrow-up"></div></div></li><li class="boolrelwidget-menu">'+settings.expand_text+'</li><li class="boolrelwidget-menu"><div class="boolrelwidget-arrow-r"><div class="boolrelwidget-arrow-up"></div></div></li></ul><ul id="boolrelwidget-collapse" class="boolrelwidget-menu-container-panel"><li class="boolrelwidget-menu"><div class="boolrelwidget-arrow-l"><div class="boolrelwidget-arrow-down"></div></div></li><li class="boolrelwidget-menu">'+settings.collapse_text+'</li><li class="boolrelwidget-menu"><div class="boolrelwidget-arrow-r"><div class="boolrelwidget-arrow-down"></div></div></li></ul><div id="boolrelwidget-panel">Hidden pannel...</div>');
+        self = self.append('<ul id="boolrelwidget-expand" class="boolrelwidget-menu-container"><li class="boolrelwidget-menu"><div class="boolrelwidget-arrow-l"><div class="boolrelwidget-arrow-up"></div></div></li><li class="boolrelwidget-menu">'+settings.expand_text+'</li><li class="boolrelwidget-menu"><div class="boolrelwidget-arrow-r"><div class="boolrelwidget-arrow-up"></div></div></li></ul><ul id="boolrelwidget-collapse" class="boolrelwidget-menu-container-panel"><li class="boolrelwidget-menu"><div class="boolrelwidget-arrow-l"><div class="boolrelwidget-arrow-down"></div></div></li><li class="boolrelwidget-menu">'+settings.collapse_text+'</li><li class="boolrelwidget-menu"><div class="boolrelwidget-arrow-r"><div class="boolrelwidget-arrow-down"></div></div></li></ul><div id="boolrelwidget-panel"><strong>Concepts</strong><div id="boolrelwidget-basicblocks">Loading...</div><strong>Boolean Query</strong><div id="boolrelwidget-query">Loading...</div></div>');
+
+
         
-        // Lets add the event handlers
+        // Lets add the event handlersx
         $( '#boolrelwidget-expand' ).click(function() {
             $('#boolrelwidget-expand').toggle();
             $('#boolrelwidget-collapse').toggle();
@@ -64,20 +207,10 @@ THE SOFTWARE.
             $('#boolrelwidget-collapse').toggle();
             $('#boolrelwidget-panel').toggle();
         });
-        return {
-            push: function (str) {
-                loose_variables.push(str);
-                console.log('Pushed new variable ' + str + ' to variables pool.');
-
-                return self;
-            },
-            pop: function (str) {
-                loose_variables.pop(str);
-                console.log('Popped variable ' + str + ' from variables pool.');
-
-                return self;
-            }
-        };
+        // Draw things up
+        funcs.draw();
+        
+        return funcs;
     };
 }(jQuery));
 
@@ -176,7 +309,16 @@ BooleanGroup.prototype = {
             }
             return returnable;
         }
-    },    
+    }, 
+    containsOnly : function(str){
+        if(this.variables.length>1)
+            return false;
+        
+        if(this.variables[0] == str)
+            return true;
+        
+        return false;
+    },
     destroy : function() {
         this.variables=null;
         this.relations=null;
