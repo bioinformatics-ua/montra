d20ebc35dbb765fb15976e3602ccb304ccd6c688
Luís A. Bastião Silva <luis.kop@gmail.com>
2014-02-14 11:40:28 +0100

    Make the Make Edit works

--- emif/emif/static/js/fingerprint_edit.js
+++ emif/emif/static/js/fingerprint_edit.js
@@ -1,17 +1,23 @@
 /* Option to active edit fingerprint option */
 if ( $("#edit_db_inputs").val() == '1' ) {
-    console.log($("#edit_db_inputs").val());
-        $("#qform input").prop("disabled", true);
-    }
+   // console.log($("#edit_db_inputs").val());
+    $("#qform input").prop("disabled", true);
+
+    $("#qform textarea").attr("disabled","disabled");
+    
+}
 
 function edit_db_option(){
 
-    console.log($("#edit_db_inputs").val());
+//    console.log($("#edit_db_inputs").val());
     if ( $("#edit_db_inputs").val() == '0' ) {
         $("#qform input").prop("disabled", true);
         $("#edit_db_inputs").val('1');
+
+	$("#qform textarea").attr("disabled","disabled");
     } else if ( $("#edit_db_inputs").val() == '1' ) {
         $("#qform input").prop("disabled", false);
+	   $("#qform textarea").removeAttr("disabled");
         $("#edit_db_inputs").val('0');
     }
 }
