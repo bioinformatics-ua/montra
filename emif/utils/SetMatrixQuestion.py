7acc0131c29eb69ab444ac7ecbcf895f131c5cb5
Tiago Marques Godinho <tmgodinho@ua.pt>
2014-02-27 18:22:59 +0000

    Now updates the slugs table as well

--- emif/utils/SetMatrixQuestion.py
+++ emif/utils/SetMatrixQuestion.py
@@ -1,4 +1,5 @@
 from questionnaire.models import *
+from searchengine.models import *
 from django.shortcuts import render_to_response, get_object_or_404
 import sys
 
@@ -15,16 +16,31 @@ def create_question(qset):
 	q = getQuestionObj(qset.id)
 	q.questionset = qset
 	q.number = desiredQN
-
-
 	# PLEASE HELP!!! CANT SET THE TEXT FIELD WHY!!	
-	#q.text = "h2. Specify any other scales"
-	
+	#q.text = "h2. Specify any other scales"	
 	q.type = "open-textfield"
 	q.help_text = "Specify each scale in a separate line. Try to follow the above questions when possible.<br>Example.  Collected, Version Cummings et al., 1994, Subgroup, Items score available."
 	q.slug = "neuropsychiatric_scales_other"
 	q.checks = "dependent=\"10.01,yes\""
 	return q
+
+def updateSlug(qs):
+	arr = Slugs.objects.filter(question=qs)
+	if len(arr) == 0:
+		x = Slugs()
+		x.question = qs
+		x.slug1 = qs.slug
+		x.description = qs.text
+		print x
+		x.save()
+		return
+	for x in arr:
+		x.slug1 = qs.slug
+		x.description = qs.text
+		print x
+		x.save()
+	return
+
 
 qsets = QuestionSet.objects.filter(heading="adcohort_Neuropsychiatric_Scales")
 print qsets
@@ -35,5 +51,6 @@ for qs in qsets:
  	question = create_question(qs)
 	question.save()
 	print "Saved Question"
+	updateSlug(question)
 
 print "QUITTING"
