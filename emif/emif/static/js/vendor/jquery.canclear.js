56d0bf56073644e6302181e2de53bc9a93ffacef
Ricardo Ribeiro <ribeiro.r@ua.pt>
2014-05-11 16:20:12 +0100

    Added the search icon on the search bar as requested. Replaced the missing refactors from old names to new ones.

--- emif/emif/static/js/vendor/jquery.canclear.js
+++ emif/emif/static/js/vendor/jquery.canclear.js
@@ -20,6 +20,7 @@
             //this.attr('class', '');
             this.attr('style','float: left; padding-top: -3px; display:inline-block; border: 0px solid #ccc !important; box-shadow: none;-moz-box-shadow: none;-webkit-box-shadow: none;');
             this.wrap('<div style="height: 28px; background-color: white; border: 1px solid #ccc; -webkit-border-radius: 15px; -moz-border-radius: 15px; border-radius: 15px;" class="canclear_outer"></div>');
+            this.parent().append('<span style="padding-right:8px; float: right; padding-top:3px; cursor: pointer;" class="canclear_search"><i class="icon-search"></i></span>');
             this.parent().append('<span style="padding-right:8px; float: right; padding-top:3px; cursor: pointer;" class="canclear_button"><i class="icon-remove"></i></span>');
             if($(this).val() == ''){
                 $(this).parent().children('.canclear_button').fadeTo(600, 0);
@@ -45,7 +46,10 @@
             $(self).val('');
             $(this).fadeTo(300, 0); 
         });    
-        
+        $(this).parent().children('.canclear_search').click(function(){
+           $(this).closest('form').submit();
+        });  
+
         }
 
         
