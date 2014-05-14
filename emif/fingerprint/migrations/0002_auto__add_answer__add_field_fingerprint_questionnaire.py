dd1ecadae5aac09e6b9c94e66c08045708f76077
Luís A. Bastião Silva <luis.kop@gmail.com>
2014-05-14 14:13:19 +0100

    [RefactorCore] Added migration schema

---
+++ emif/fingerprint/migrations/0002_auto__add_answer__add_field_fingerprint_questionnaire.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Answer'
+        db.create_table('fingerprint_answer', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('question', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['questionnaire.Question'])),
+            ('data', self.gf('django.db.models.fields.TextField')()),
+            ('comment', self.gf('django.db.models.fields.TextField')()),
+            ('fingerprint_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['fingerprint.Fingerprint'])),
+        ))
+        db.send_create_signal('fingerprint', ['Answer'])
+
+        # Adding field 'Fingerprint.questionnaire'
+        db.add_column('fingerprint_fingerprint', 'questionnaire',
+                      self.gf('django.db.models.fields.related.ForeignKey')(to=orm['questionnaire.Questionnaire'], null=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting model 'Answer'
+        db.delete_table('fingerprint_answer')
+
+        # Deleting field 'Fingerprint.questionnaire'
+        db.delete_column('fingerprint_fingerprint', 'questionnaire_id')
+
+
+    models = {
+        'fingerprint.answer': {
+            'Meta': {'object_name': 'Answer'},
+            'comment': ('django.db.models.fields.TextField', [], {}),
+            'data': ('django.db.models.fields.TextField', [], {}),
+            'fingerprint_id': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['fingerprint.Fingerprint']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'question': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['questionnaire.Question']"})
+        },
+        'fingerprint.fingerprint': {
+            'Meta': {'object_name': 'Fingerprint'},
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'fingerprint_hash': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'questionnaire': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['questionnaire.Questionnaire']", 'null': 'True'})
+        },
+        'questionnaire.question': {
+            'Meta': {'object_name': 'Question'},
+            'category': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'checks': ('django.db.models.fields.CharField', [], {'max_length': '128', 'null': 'True', 'blank': 'True'}),
+            'extra_en': ('django.db.models.fields.CharField', [], {'max_length': '128', 'null': 'True', 'blank': 'True'}),
+            'footer_en': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'help_text': ('django.db.models.fields.CharField', [], {'max_length': '2255', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'number': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'questionset': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['questionnaire.QuestionSet']"}),
+            'slug': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'slug_fk': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['searchengine.Slugs']", 'null': 'True', 'blank': 'True'}),
+            'stats': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'text_en': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'tooltip': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '32'})
+        },
+        'questionnaire.questionnaire': {
+            'Meta': {'object_name': 'Questionnaire'},
+            'disable': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'redirect_url': ('django.db.models.fields.CharField', [], {'default': "'/static/complete.html'", 'max_length': '128'}),
+            'slug': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        'questionnaire.questionset': {
+            'Meta': {'object_name': 'QuestionSet'},
+            'checks': ('django.db.models.fields.CharField', [], {'max_length': '128', 'blank': 'True'}),
+            'heading': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'help_text': ('django.db.models.fields.CharField', [], {'max_length': '2255', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'questionnaire': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['questionnaire.Questionnaire']"}),
+            'sortid': ('django.db.models.fields.IntegerField', [], {}),
+            'text_en': ('django.db.models.fields.TextField', [], {}),
+            'tooltip': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        'searchengine.slugs': {
+            'Meta': {'object_name': 'Slugs'},
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slug1': ('django.db.models.fields.CharField', [], {'max_length': '1256'})
+        }
+    }
+
+    complete_apps = ['fingerprint']
