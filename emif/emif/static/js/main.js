58d4624f099508d987d50b7c7e10a6a3e0770e5e
Ricardo Ribeiro <ribeiro.r@ua.pt>
2014-05-27 17:04:04 +0100

    Don't allow empty quick search queries

--- emif/emif/static/js/main.js
+++ emif/emif/static/js/main.js
@@ -1,1 +1,11 @@
 
+// Validation of quicksearch
+$('#quicksearch').submit(function() {
+
+        var quick_search = $('#edit-search-block-form--3', $(this)).val().trim();
+
+        if(!quick_search || quick_search.length == 0)
+            return false;
+
+        return true;
+});
