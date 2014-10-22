45d72e7d6ae32c1af316145560bd01cd9b25b8de
Luís A. Bastião Silva <luis.kop@gmail.com>
2014-10-22 21:44:20 +0100

    Just put it working

--- emif/questionnaire/export.py
+++ emif/questionnaire/export.py
@@ -18,6 +18,20 @@
 #
 
 from questionnaire.models import *
+
+
+def removehs(value):
+    value = value.replace('h0. ','')
+    value = value.replace('h1. ','')
+    value = value.replace('h2. ','')
+    value = value.replace('h3. ','')
+    value = value.replace('h4. ','')
+    value = value.replace('h5. ','')
+    value = value.replace('h6. ','')
+    value = value.replace('h7. ','')
+    return value
+
+
 """This class is used to export the fingerprint template to excel
 """
 class ExportFingerprint(object):
@@ -33,13 +47,22 @@ class ExportFingerprint(object):
     def export(self):
         raise NotImplementedError("Please Implement this method")
 
+
+    def level(self, question):
+        return question.number.count(".") + 1
+
+
+    def clean(self, text):
+        return removehs(text)
+
+
     """This method will build the object according with the type
     of the object to export.
     """
     @staticmethod
     def factory(t_type, questionnaire):
         if t_type == "csv_plain":
-            pass
+            return ExportFingerprintCSVPlain(questionnaire)
         else:
             raise Exception("The supplied format is not supported")
 
@@ -47,18 +70,46 @@ class ExportFingerprint(object):
 class ExportFingerprintCSVPlain(ExportFingerprint):
 
     def __init__(self, questionnaire ):
-        super.__init__(self, questionnaire)
-        pass
+        ExportFingerprint.__init__(self, questionnaire)
 
     def ignore_questionsets(self, questionset_list):
         raise NotImplementedError("Please Implement this method")
 
+    def get_tabs(self, level):
+
+        return "\t" * level
+
     def export(self):
         result = ""
-        questionsets = Questionnaire.questionsets()
+        questionsets = self.questionnaire.questionsets()
 
         for qs in questionsets:
 
-            qs.questions()
+            result += str(qs.sortid) + " - " + self.clean(qs.text)  + "\n"
+            questions = qs.questions()
+            for q in questions:
+                _level  = self.level(q)
+                result += self.get_tabs(_level)
+                result += str(q.number)  + " " + self.clean(q.text) +"\n"
+
+
+        f = open('/tmp/csvplain.csv','w')
+        f.write(result)
+        f.close()
+
+
+def main():
+
+    q = Questionnaire.objects.get(id=53)
+
+    exporter = ExportFingerprint.factory("csv_plain", q)
+    exporter.export()
+
+
+
+
+
+
+
 
 
