a088d37d36f39738a2d310a51a763b5ab7cec0f7
Ricardo Ribeiro <ribeiro.r@ua.pt>
2014-11-11 01:39:48 +0000

    Added categories different layout on database comparison.

--- emif/questionnaire/services.py
+++ emif/questionnaire/services.py
@@ -52,7 +52,7 @@ def creatematrixqsets(db_type, fingerprints, qsets):
                 else:
                     a_list.append([answer.data, question.type])
             except Answer.DoesNotExist:
-                a_list.append("")
+                a_list.append(["", question.type])
 
         name = "Unnamed"
 
