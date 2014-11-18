599e8f136985ed471490cdae470614b9bbe6a752
Ricardo Ribeiro <ribeiro.r@ua.pt>
2014-11-18 15:26:55 +0000

    Clearing value on input for advanced search tests, since IE doesnt work properly on this.

--- tests/search/2_advancedsearch.js
+++ tests/search/2_advancedsearch.js
@@ -12,6 +12,7 @@ module.exports = {
       .click('#actions a[href="advancedSearch/49/1"]')
       .waitForElementVisible('li[id="li_qs_1"]', 10000, 'Advanced search loads with success')
       .waitForElementVisible('input[id="question_1.01"]', 10000, 'Questionset 1 Loaded with success')
+      .clearValue('input[id="question_1.01"]')
       .setValue('input[id="question_1.01"]', 'ARS')
       .click('button[type=submit]')
       .waitForElementVisible('a[data-acronym="ARS"]', 10000, 'Search Returned with success')
